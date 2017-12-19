ded1cbd252776fad15b778afd41c461f6de7df22
Kaito Kishi <kaito@kishi.com>
2017-12-20 00:01:35 +0900

    Fix two-phase simplex method.

--- emd.js
+++ emd.js
@@ -8,6 +8,9 @@ var emd = function(wp, wq, d, use_subscript) {
         return 0;
     }), []];
     var consts = [0, 0];
+    var subscripts = Array.apply(null, Array(wp.length * 2 + 2)).map(function(value, index) {
+        return wp.length * 2 + index - 2;
+    });
     for(var i = 0; i < d.length; ++i) {
         for(var j = 0; j < d[i].length; ++j) {
             table[1].push(d[i][j]);
@@ -44,6 +47,77 @@ var emd = function(wp, wq, d, use_subscript) {
             return 0;
         })));
         consts.push(wq[i]);
+    }
+
+    for(var i = 2; i < wp.length * 2 + 2; ++i) {
+        table[0] = table[0].map(function(value, index) {
+            return value - table[i][index];
+        });
+    }
+
+    var state = 0;
+    while(true) {
+        var selected_col_index = null;
+        var selected_col_subscript = null;
+        for(var i = 0; i < table[0].length; ++i) {
+            if(table[0][i] < 0 && (selected_col_subscript == null || subscripts[i] < selected_col_subscript)) {
+                selected_col_subscript = subscripts[i];
+                selected_col_index = i;
+            }
+        }
+        if(selected_col_index == null) {
+            if(state == 0) {
+                state = 1;
+                table = table.slice(1);
+                for(var i = 0; i < table.length; ++i) {
+                    table[i] = table[i].slice(0, length);
+                }
+                consts = consts.slice(1);
+                continue;
+            }
+
+            return -consts[0];
+        }
+
+        var selected_row_index = null;
+        var selected_divided_const = null;
+        var selected_row_subscript = null;
+        for(var i = 2; i < table.length; ++i) {
+            if(table[i][selected_col_index] <= 0) {
+                continue;
+            }
+            if(selected_row_index == null ||
+                    consts[i] / table[i][selected_col_index] < selected_divided_const ||
+                    (consts[i] / table[i][selected_col_index] == selected_divided_const && subscripts[i] < selected_row_subscript)) {
+                selected_row_subscript = subscripts[i];
+                selected_row_index = i;
+                selected_divided_const = consts[i] / table[i][selected_col_index];
+            }
+        }
+        if(selected_row_index == null) {
+            throw new Error('selected_row_index == null!');
+        }
+
+        var origin_coefficient = table[selected_row_index][selected_col_index];
+        table[selected_row_index] = table[selected_row_index].map(function(value) {
+            return value / origin_coefficient;
+        });
+        consts[selected_row_index] /= origin_coefficient;
+
+        for(var i = 0; i < table.length; ++i) {
+            if(i == selected_row_index) {
+                continue;
+            }
+            var coefficient = table[i][selected_col_index];
+            table[i] = table[i].map(function(value, index) {
+                return value - table[selected_row_index][index] * coefficient;
+            });
+            consts[i] -= consts[selected_row_index] * coefficient;
+        }
+
+        var tmp = subscripts[selected_col_index];
+        subscripts[selected_col_index] = subscripts[selected_row_index - 2 + wp.length * 2];
+        subscripts[selected_row_index - 2 + wp.length * 2] = tmp;
     }
 
     var jj = 0;
@@ -167,7 +241,7 @@ var emd = function(wp, wq, d, use_subscript) {
         var min_divided_const = null;
         var min_row_index = null;
         for(var i = (state == 2 ? 1 : 2); i < table.length; ++i) {
-            if(table[i][min_col_index] == 0) {
+            if(table[i][min_col_index] <= 0) {
                 continue;
             }
             var tmp = consts[i] / table[i][min_col_index];
